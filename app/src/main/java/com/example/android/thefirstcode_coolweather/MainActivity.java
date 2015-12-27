b9e2c382ea0c00f9d622e8be27f70cb7264f5ee1
lizhaocheng <lizhaocheng126@163.com>
2015-12-28 06:49:27 +1100

    First commit

---
+++ app/src/main/java/com/example/android/thefirstcode_coolweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.thefirstcode_coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        http://api.map.sina.com.cn/base/weather.json?city_name=北京&source=2699414926
+    }
+}
